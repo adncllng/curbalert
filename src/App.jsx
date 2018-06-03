103434bd6743f93a5677793a90440f334221dc64
Aden Collinge <aden.collinge@gmail.com>
2018-06-03 17:42:34 -0400

    added router-route for /Upload (newpost)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import './styles/scss/App.css';
 import Home from "./Home.jsx";
 import NavBar from "./NavBar.jsx";
+import NewPost from "./NewPost.jsx"
 
 class App extends Component {
 	render() {
@@ -11,6 +12,9 @@ class App extends Component {
 				<NavBar />
 				<Switch>
 					<Route exact path="/" component={Home} />
+					<Route exact path='/upload' render={() => (
+						<NewPost trashUploadHandler={this.trashUploadHandler} addPost={this.addPost} />
+					)}/>
 					{/* both /roster and /roster/:number begin with /roster */}
 				</Switch>
 			</div>
