462aab6e194532621eb5b09615e5ec0125ebf721
Aden Collinge <aden.collinge@gmail.com>
2018-06-03 15:25:19 -0400

    many changes

--- crblrt-server/app.js
+++ crblrt-server/app.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3002");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
--- src/App.js
+++ src/App.js
@@ -3,13 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './home.jsx';
+import Upload from './upload.jsx';
 
 class App extends Component {
+  state = {
+    trashFile: "",
+    posts: null
+  }
+
+  trashUploadHandler = event => {
+    console.log("somehing happens")
+    this.setState({
+      trashFile: event.target.files[0]
+    })
+  }
+
+   addPost = post => {
+     console.log("somehing happens")
+     const updatedPosts = this.state.posts.concat(post)
+     this.setState({
+       posts: updatedPosts
+     })
+   }
+
   render() {
     return (
 <div>
 <Switch>
-  <Route exact path='/' component={Home}/>
+  <Route path='/' render={() => (
+    <Upload trashUploadHandler={this.trashUploadHandler} addPost={this.addPost} />
+  )}/>
   {/* both /roster and /roster/:number begin with /roster */}
 </Switch>
 </div>
