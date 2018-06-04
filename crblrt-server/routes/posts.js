abb840ee70ec783543b4b1926811be8a3e9ee7b7
Aden Collinge <aden.collinge@gmail.com>
2018-06-04 17:12:49 -0400

    routes for post to posts and knex queries

--- crblrt-server/routes/posts.js
+++ crblrt-server/routes/posts.js
@@ -1,43 +1,44 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 
 module.exports = (knex) => {
-
-  router.post("/", (req, res) => {
-
-  const {user_id,title,content,image_url,geo_tag,point_value,visible,tags} = req.body;
-  let postId = null;
-  knex('posts')
+  router.post('/', (req, res) => {
+    const {
+      user_id, title, content, image_url, geo_tag, point_value, visible, tags,
+    } = req.body;
+    let postId = null;
+    knex('posts')
       .returning('id')
-      .insert({user_id,title,content,image_url,geo_tag,point_value,visible})
-      .then((post_id)=>{
+      .insert({
+        user_id,
+        title,
+        content,
+        image_url,
+        geo_tag,
+        point_value,
+        visible,
+      })
+      .then((post_id) => {
         postId = post_id;
-              knex('tags')
-                .returning('id')
-                .insert(
-                  tags.map(tag => {
-                    return {name: tag};
-                  })
-                )
-                .then((tag_ids)=>{
-                  knex('posts_tags')
-                  .insert(
-                    tag_ids.map(tagId =>{
-                      return {post_id: Number(postId), tag_id: Number(tagId)};
-                    })
-                  ).then(res.send("fucking hell"))
-                })
-    })
+        knex('tags')
+          .returning('id')
+          .insert(tags.map(tag => ({ name: tag })))
+          .then((tag_ids) => {
+            knex('posts_tags')
+              .insert(tag_ids.map(tagId => ({ post_id: Number(postId), tag_id: Number(tagId) })))
+              .then(res.send('fucking hell'));
+          });
+      });
   });
 
-
-  router.get("/", (req, res) => {
+  router.get('/', (req, res) => {
     knex
-      .select("*")
-      .from("posts_tags")
+      .select('*')
+      .from('')
       .then((results) => {
-        res.json(results)
+        res.json(results);
       });
   });
   return router;
-}
+};
