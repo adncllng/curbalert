54ab7e472a4af3079caa48b16a34b0346e0cbc42
Aden Collinge <aden.collinge@gmail.com>
2018-06-04 17:09:12 -0400

    added knex queries for adding posts

--- crblrt-server/routes/posts.js
+++ crblrt-server/routes/posts.js
@@ -4,10 +4,37 @@ var router = express.Router();
 module.exports = (knex) => {
 
   router.post("/", (req, res) => {
-    console.log(req)
+
+  const {user_id,title,content,image_url,geo_tag,point_value,visible,tags} = req.body;
+  let postId = null;
+  knex('posts')
+      .returning('id')
+      .insert({user_id,title,content,image_url,geo_tag,point_value,visible})
+      .then((post_id)=>{
+        postId = post_id;
+              knex('tags')
+                .returning('id')
+                .insert(
+                  tags.map(tag => {
+                    return {name: tag};
+                  })
+                )
+                .then((tag_ids)=>{
+                  knex('posts_tags')
+                  .insert(
+                    tag_ids.map(tagId =>{
+                      return {post_id: Number(postId), tag_id: Number(tagId)};
+                    })
+                  ).then(res.send("fucking hell"))
+                })
+    })
+  });
+
+
+  router.get("/", (req, res) => {
     knex
       .select("*")
-      .from("posts")
+      .from("posts_tags")
       .then((results) => {
         res.json(results)
       });
