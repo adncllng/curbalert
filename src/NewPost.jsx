574e1b200600bddc255b6bc231ce24829d1848e3
Aden Collinge <aden.collinge@gmail.com>
2018-06-05 16:47:08 -0400

    tidy up

--- src/NewPost.jsx
+++ src/NewPost.jsx
@@ -182,47 +182,6 @@ addTag = event => {
                    </p>
                  </div>
 
-                 <Card>
-                 <Card.Image size="4by3" src="http://bulma.io/images/placeholders/1280x960.png" />
-                 <Card.Content>
-                 <Media>
-                 <Media.Item renderAs="figure" position="left">
-                 <Image
-                   renderAs="p"
-                   size={64}
-                   alt="64x64"
-                   src="http://bulma.io/images/placeholders/128x128.png"
-                 />
-                 </Media.Item>
-                 <Media.Item>
-                 <Heading size={4}>
-                 John Smith
-                 </Heading>
-                 <Heading subtitle size={6}>
-                 @johnsmith
-                 </Heading>
-                 </Media.Item>
-                 </Media>
-                 <Content>
-                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-                 <a>
-                 @bulmaio
-                 </a>
-                 .
-                 <a href="#1">
-                 #css
-                 </a>
-
-                 <a href="#2">
-                 #responsive
-                 </a>
-                 <br/>
-                 <time dateTime="2016-1-1">
-                 11:09 PM - 1 Jan 2016
-                 </time>
-                 </Content>
-                 </Card.Content>
-                 </Card>
 
 
 
