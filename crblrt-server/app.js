bfc78a2a3f879b737815b4c504ee27f3809e084a
Nina Yujiri <ninayujiri@gmail.com>
2018-06-05 00:34:06 +0000

    changed to google-map-react and rendered markers as dynamic componentsgit add .

--- crblrt-server/app.js
+++ crblrt-server/app.js
@@ -1,7 +1,6 @@
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
-// var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
@@ -23,7 +22,6 @@ var app = express();
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(function(req, res, next) {
