3b0255dcc999b80aba79e4854c4041a1d71b8db2
Aden Collinge <aden.collinge@gmail.com>
2018-06-06 15:44:32 -0400

    adds tags to get /posts

--- crblrt-server/routes/posts.js
+++ crblrt-server/routes/posts.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 module.exports = (knex) => {
   router.post('/', (req, res) => {
+    console.log()
 
     const {
       user_id, title, content, image_url, geo_tag, point_value, visible, tags,
@@ -18,7 +19,7 @@ module.exports = (knex) => {
         title,
         content,
         image_url,
-        geo_tag: `${geo_tag.x}, ${geo_tag.y}`,
+        geo_tag,
         point_value,
         visible,
       })
@@ -40,8 +41,32 @@ module.exports = (knex) => {
     knex
       .select('*')
       .from('posts')
-      .then((results) => {
-        res.json(results);
+      // .leftJoin('posts_tags','posts.id','posts_tags.post_id')
+      // .leftJoin('tags','posts_tags.tag_id','tags.id')
+      .then((posts) => {
+        // [{post}]
+        let postIds = posts.map(result => result.id)
+
+        knex
+        .select('posts_tags.post_id', 'tags.name')
+        .from('posts_tags')
+        .join('tags', 'tags.id','posts_tags.tag_id')
+        .whereIn('posts_tags.post_id',postIds)
+        .whereNotNull('tags.name')
+        .then(postIdsAndTagNames =>{
+        res.json(posts.map(post => {
+          console.log(post)
+            let tagsArray =[]
+             postIdsAndTagNames.forEach( postIdAndTagName => {
+             if (post.id == postIdAndTagName.post_id ){
+                tagsArray.push(postIdAndTagName.name)}
+            })
+            return {...post, tags: tagsArray}
+          })
+        )
+
+
+        })
       });
   });
   return router;
