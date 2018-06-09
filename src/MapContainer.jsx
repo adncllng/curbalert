eb1268db129620799661d92cb6a47d771e5e71c3
Aden Collinge <aden.collinge@gmail.com>
2018-06-08 21:13:05 -0400

    minor changes

--- src/MapContainer.jsx
+++ src/MapContainer.jsx
@@ -16,7 +16,7 @@ class MapContainer extends Component {
 
   toggleModal = key => {
     let thisPost = null;
-      this.state.posts.forEach((post, i) => {
+      this.props.posts.forEach((post, i) => {
       if (post.id == key) {
        thisPost = post
       }
@@ -25,7 +25,7 @@ class MapContainer extends Component {
   }
 
   render() {
-    const markers = this.state.posts.map(marker =>
+    const markers = this.props.posts.map(marker =>
       <Marker
         key={marker.id}
         lat={marker ? marker.geo_tag.x : ''}
